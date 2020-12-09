47fc4dd1be3ce9b02e5347c2e0b5bac64f4b7565
Luiz Carvalho <Luiz_Carvalho1@Dellteam.com>
2020-12-09 09:45:27 -0300

    add pet service

--- services/grade.service.js
+++ services/grade.service.js
@@ -1,9 +1,41 @@
 const createGrade = (content) => {
+    var reading = content.Reading;
+    var writing = content.Writing;
+    var listening = content.Listening;
+    var speaking = content.Speaking;
+
+    var resultReading = (reading * 25) / 32;
+    var percentReading = resultReading / 25;
+
+    var resultWriting = (writing * 25) / 40;
+    var percentWriting = resultWriting / 25;
+    
+    var resultListening = (listening * 25) / 25;
+    var percentListening = resultListening / 25;
+
+    var resultSpeaking = (speaking * 25) / 25;
+    var percentSpeaking = resultSpeaking / 25;
+
+    var total = (resultWriting + resultReading + resultListening + resultSpeaking);
+    
     return {
-        age: content.Age,
-        cpf: content.Cpf,
-        email: content.Email,
-        name: content.Name
+        reading: {
+            result: resultReading,
+            percent: percentReading
+        },
+        writing: {
+            result: resultWriting,
+            percent: percentWriting
+        },
+        listening: {
+            result: resultListening,
+            percent: percentListening
+        },
+        speaking: {
+            result: resultSpeaking,
+            percent: percentSpeaking
+        },
+        total: total
     };
 }
 
