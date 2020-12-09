97141b108896fd1baa928efff6a77d1229eb4000
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2020-12-09 02:04:03 -0300

    add swagger

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const routes = require('./routes')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/', (req, res) => res.send('App is working'))
+app.get('/', (req, res) => res.send('App is working'));
 
-app.use('/api', routes)
+app.use('/api', routes);
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
 
 module.exports = {
   app
